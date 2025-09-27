b56863b9a3437d2e7588bf7e8533b3173c055909
AJVines <austin.vines@bison.howard.edu>
2025-09-26 23:19:56 -0400

    i have an MVP 9/26

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -4,13 +4,16 @@ var zombie_scene: PackedScene
 @onready var player = get_node("Player")
 @onready var camera = get_node("Player/Camera2D")
 
+var wave_active = false
+var wave_round = 0
+
 func _ready():
 	# Load the zombie scene programmatically
 	zombie_scene = load("res://Scenes/zombie.tscn")
 	if zombie_scene == null:
 		push_error("Failed to load zombie scene at 'res://Scenes/zombie.tscn'")
 		return
-	zombie_cycle()
+	launch_wave()
 	
 	player.connect("playerdied", _on_player_died)
 
@@ -24,9 +27,28 @@ func zombie_cycle():
 		
 		zombie_instance.connect("zombiedied", _on_zombie_died) #connect the zombie to signal
 	
+		print("[Level] Zombie Spawned")
+		#await get_tree().create_timer(3.0).timeout
 		add_child(zombie_instance)
-		await get_tree().create_timer(3.0).timeout
+		# zombie_cycle()
+		#zombie cycle no longer cycles itself
+
+func launch_wave():
+	wave_round = wave_round + 1
+	$Player/Camera2D/wave_label.text = "Wave: " + str(wave_round)
+	print("[Level] New wave")
+	wave_active = true
+	for j in range(10 * wave_round):
 		zombie_cycle()
+		await get_tree().create_timer(3.0).timeout
+	launch_wave()
+	
+
+func _process(delta):
+	if get_tree().get_nodes_in_group("Zombie").size() == 0 and not wave_active:
+		launch_wave()
+	elif get_tree().get_nodes_in_group("Zombie").size() > 0:
+		wave_active = false
 
 func _on_player_died():
 	set_physics_process(false)
